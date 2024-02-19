fa72f53f4a334ee00516177b2f57270466b536a7
sigmotoa <si.galvismotoa@gmail.com>
2024-02-19 16:43:38 -0500

    Cuadrado de un numero

---
+++ Repaso1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hola a todes!");
+var n=1.1;
+var square_n=0.0;
+
+Console.WriteLine("Introduzca valor para operar");
+
+n=Convert.ToDouble(Console.ReadLine());
+square_n = n * n;
+Console.WriteLine($"El valor de {n} al cuadrado es {square_n}");
